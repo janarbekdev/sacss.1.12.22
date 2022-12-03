38429e2e7d9624bedd78117b12f4f07021653d6a
janarbekdev <zanarbekm91@gmail.com>
2022-12-03 16:15:08 +0300

    joni

---
+++ gs/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
